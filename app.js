99bc4fde23eb1249a1a1fb80ca7fd7ec2444d95b
Kozo Oshima <ok15st@gmail.com>
2018-09-09 13:35:50 +0000

    メモ化による改善

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 'use strict';
+
+const meno = new Map();
+meno.set(0, 0);
+meno.set(1, 1);
 function fib(n){
-  if (n === 0){
-    return 0;
-  } else if(n === 1) {
-      return 1;
-  }
-  return fib(n-1) + fib(n-2);
+    if(meno.has(n)){
+        return meno.get(n);
+    }
+    const value = fib(n-1) + fib(n-2);
+    meno.set(n, value);
+    return value;
+}
+const length = 40;
+for (let i = 0; i <= length; i++){
+    console.log(fib(i));
 }
 
-const length = 40;
-for (let i = 0; i<= length; i++){
-  console.log(fib(i))
-}
+//指数オーダーの関数
+//function fib(n){
+//  if (n === 0){
+//    return 0;
+//  } else if(n === 1) {
+//      return 1;
+//  }
+//  return fib(n-1) + fib(n-2);
+//}
+//
+//const length = 40;
+//for (let i = 0; i<= length; i++){
+//  console.log(fib(i))
+//}
